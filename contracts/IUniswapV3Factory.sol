5bd1e7dc2d5639c216e448162223ab293c1234a6
Ihor.A <i.antsupov@gmail.com>
2024-05-31 08:44:42 +0300

    feat: Add Uniswap V3 and MyToken contracts

---
+++ contracts/IUniswapV3Factory.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+interface IUniswapV3Factory {
+    function createPool(
+        address tokenA,
+        address tokenB,
+        uint24 fee
+    ) external returns (address pool);
+
+    function getPool(
+        address tokenA,
+        address tokenB,
+        uint24 fee
+    ) external view returns (address pool);
+}
